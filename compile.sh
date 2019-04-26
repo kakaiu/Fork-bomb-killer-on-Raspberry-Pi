5b31157de5c7dd6a857d9b08618c3202821feacb
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-04-26 15:50:41 -0500

    f

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@
 module add raspberry
 KERNEL=kernel7
 echo $KERNEL
-LINUX_SOURCE=/tmp/compile/qitao/linux_source/linux
+LINUX_SOURCE=/tmp/compile/zhe.wang/linux_source/linux
 echo $LINUX_SOURCE
 make -C $LINUX_SOURCE ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- SUBDIRS=$PWD modules
