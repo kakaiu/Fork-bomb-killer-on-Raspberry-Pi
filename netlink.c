74cef8161a5204ff7297b113b1d6312d78618d97
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-04-26 21:12:50 -0500

    g

--- netlink.c
+++ netlink.c
@@ -86,8 +86,8 @@ static int init_global(Global_data* g_ptr) {
 
 static int get_proc_stat(char* buffer, int size) {
 	struct file *f;
-	printk("get_proc_stat");
 	mm_segment_t fs;
+	printk("get_proc_stat");
 	f = filp_open("/proc/stat", O_RDONLY, 0);
 	if(f == NULL){
 		printk(KERN_ALERT "filp_open error!!.\n");
