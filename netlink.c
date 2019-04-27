f61f7610016b4ffc67706ad61ec6bca65eedc33d
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-04-26 19:21:07 -0500

    f

--- netlink.c
+++ netlink.c
@@ -15,7 +15,6 @@
 #include <linux/slab.h>
 #define NETLINK_TEST 17 
 const char d[2] = " ";
-static struct sock *socket_ptr = NULL;
 static unsigned long period_sec= 1;
 static unsigned long period_nsec=0;
 static struct hrtimer hr_timer;
@@ -23,102 +22,96 @@ static ktime_t interval;
 static struct task_struct *thread1;
 long prev_idle=0;
 long prev_total=0;
-// static char *statCPU="/proc/stat";
-struct sched_param{
+
+struct sched_param {
 	int sched_priority;
 };
+
+struct global_data {
+	int buffer_size;
+	char* buffer;
+	char* token;
+	static struct sock *socket_ptr;
+};
+
+struct global_data g;
+
 module_param(period_sec,ulong, 0);
 module_param(period_nsec, ulong, 0);
 
 static void netlink_recv_msg(struct sk_buff * skb) {
-
     struct nlmsghdr * nlh = NULL;
-
     if (skb == NULL) {
         printk(KERN_INFO "skb is NULL \n");
-
-        return ;
+        return;
     }
-
     nlh = (struct nlmsghdr *)skb->data;
     printk(KERN_INFO "%s: received netlink message payload: %s\n", __FUNCTION__, (char *)NLMSG_DATA(nlh));
-
 }
 
 struct netlink_kernel_cfg cfg = {
     .input = netlink_recv_msg,
 };
-/* static function for kernel thread*/
 
-// struct file *file_open(const char *path, int flags, int rights) 
-// {
-//     struct file *filp = NULL;
-//     mm_segment_t oldfs;
-//     int err = 0;
+static int init_buffer(char* buffer, int size) {
+	buffer = kmalloc_array(sizeof(char*), size, GFP_KERNEL);
+    if (!buffer) {
+    	printk(KERN_ALERT "Allocation error!!.\n");
+    	return -1;
+    }
+    return 0;
+}
 
-//     oldfs = get_fs();
-//     set_fs(get_ds());
-//     filp = filp_open(path, flags, rights);
-//     set_fs(oldfs);
-//     if (IS_ERR(filp)) {
-//         err = PTR_ERR(filp);
-//         return NULL;
-//     }
-//     return filp;
-// }
-// //Close a file (similar to close):
+static int init_token(char* token) {
+	token = kmalloc(sizeof(char*), GFP_KERNEL);
+    if (!token) {
+    	printk(KERN_ALERT "Allocation error!!.\n");
+    	return -1;
+    }
+    return 0;
+}
 
-// void file_close(struct file *file) 
-// {
-//     filp_close(file, NULL);
-// }
-// //Reading data from a file (similar to pread):
+static int init_global() {
+	g.buffer_size = 1024;
+	if (init_buffer(g.buffer, g.buffer_size)==-1) {
+		return -1;
+	}
+	if (init_token(g.token)==-1) {
+		return -1;
+	}
+	socket_ptr = NULL
+	return 0;
+}
 
-// int file_read(struct file *file, unsigned long long offset, unsigned char *data, unsigned int size) 
-// {
-//     mm_segment_t oldfs;
-//     int ret;
-
-//     oldfs = get_fs();
-//     set_fs(get_ds());
-
-//     ret = vfs_read(file, data, size, &offset);
-
-//     set_fs(oldfs);
-//     return ret;
-// }   
-// //Writing data to a file (similar to pwrite):
-
-// int file_write(struct file *file, unsigned long long offset, unsigned char *data, unsigned int size) 
-// {
-//     mm_segment_t oldfs;
-//     int ret;
-
-//     oldfs = get_fs();
-//     set_fs(get_ds());
-
-//     ret = vfs_write(file, data, size, &offset);
-
-//     set_fs(oldfs);
-//     return ret;
-// }
-// //Syncing changes a file (similar to fsync):
-
-// int file_sync(struct file *file) 
-// {
-//     vfs_fsync(file, 0);
-//     return 0;
-// }
-
+static int get_proc_stat(char* buffer, int size) {
+	f = filp_open("/proc/stat", O_RDONLY, 0);
+	if(f == NULL){
+		printk(KERN_ALERT "filp_open error!!.\n");
+		return -1;
+	} else {
+		// Get current segment descriptor
+		fs = get_fs();
+		// Set segment descriptor associated to kernel space
+		set_fs(get_ds());
+		// Read the file
+		f->f_op->read(f, buffer, size, &f->f_pos);
+		// Restore segment descriptor
+		set_fs(fs);
+		// See what we read from file
+		printk(KERN_INFO "buf:%s",buffer);
+		return 0;
+	}
+	filp_close(f, NULL);
+}
 
 //https://stackoverflow.com/questions/1184274/read-write-files-within-a-linux-kernel-module
-static int thread_fn(void * data){
-	int i;
+static int thread_fn(void * data) {
+	int i, ret;
 	struct file *f;
-	int ret;
-    // char buf[1024];
     char* token;
     char* buffer;
+    int buffer_size = buffer_size;
+
     long total;
     long total_idle;
     long idle;
@@ -127,76 +120,46 @@ static int thread_fn(void * data){
     mm_segment_t fs;
 
     socket_ptr = netlink_kernel_create(&init_net, NETLINK_TEST, &cfg);
-    printk(KERN_INFO "Initializing Netlink Socket\n");
-    token=kmalloc(sizeof(char*),GFP_KERNEL);
-    if (!token)
-    	printk(KERN_ALERT "Allocation error!!.\n");
-    buffer=kmalloc_array(sizeof(char*),1024,GFP_KERNEL);
-    if (!buffer)
-    	printk(KERN_ALERT "Allocation error!!.\n");
-    // Init the buffer with 0
-    // memset(buf, 0, 1024);
-    // To see in /var/log/messages that the module is operating
-    printk(KERN_INFO "My module is loaded\n");
-    // I am using Fedora and for the test I have chosen following file
-    // Obviously it is much smaller than the 128 bytes, but hell with it =)
+    if (init_global()==-1) {
+    	return 0;
+    }
     
 	while (!kthread_should_stop()){ 
 		set_current_state(TASK_INTERRUPTIBLE);
   		schedule();
-		f = filp_open("/proc/stat", O_RDONLY, 0);
-	    if(f == NULL)
-	        printk(KERN_ALERT "filp_open error!!.\n");
-	    else{
-	        // Get current segment descriptor
-	        fs = get_fs();
-	        // Set segment descriptor associated to kernel space
-	        set_fs(get_ds());
-	        // Read the file
-	        f->f_op->read(f, buffer, 1024, &f->f_pos);
-	        // Restore segment descriptor
-	        set_fs(fs);
-	        // See what we read from file
-	        printk(KERN_INFO "buf:%s",buffer);
-	    }
-	    filp_close(f,NULL);
-	    total=0;
-	    idle=0;
-	    i=0;
-	    percentage=0;
-	    total_idle=0;
-	    // buffer=&buf;
-	    while( (token = strsep(&buffer,d)) != NULL &&i<10){
-        	printk(KERN_INFO "%s %d \n",token,i);
-        	if(i!=0&&i!=1){
-        		ret=kstrtol(token,10,&split);
-    	 		if(ret!=0)
-    	 			printk(KERN_ALERT "Conversion error!!.\n");
-    	 		total=total+split;
-    	 	}
-    	 	
-    	 	if(i==5||i==6){
-    	 		ret=kstrtol(token,10,&idle);
-    	 		if(ret!=0)
-    	 			printk(KERN_ALERT "Conversion error!!.\n");
-    	 		total_idle+=idle;
-	    	}
-        	i+=1;
-	    }
-	   
-	    // percentage=(1.0 - (idle-prev_idle)*1.0/(total-prev_total))*100;
-	   	prev_idle=total_idle;
-	   	prev_total=total;
-
-      	printk(KERN_INFO "In thread1 %ld %ld",idle,total);
+		if (get_proc_stat(g.buffer, g.buffer_size)==-1) {
+			continue;
+		} else {
+			/*total=0;
+		    idle=0;
+		    i=0;
+		    percentage=0;
+		    total_idle=0;
+		    // buffer=&buf;
+		    while( (token = strsep(&buffer,d)) != NULL &&i<10){
+	        	printk(KERN_INFO "%s %d \n",token,i);
+	        	if(i!=0&&i!=1){
+	        		ret=kstrtol(token,10,&split);
+	    	 		if(ret!=0)
+	    	 			printk(KERN_ALERT "Conversion error!!.\n");
+	    	 		total=total+split;
+	    	 	}
+	    	 	
+	    	 	if(i==5||i==6){
+	    	 		ret=kstrtol(token,10,&idle);
+	    	 		if(ret!=0)
+	    	 			printk(KERN_ALERT "Conversion error!!.\n");
+	    	 		total_idle+=idle;
+		    	}
+	        	i+=1;
+		    }
+		    // percentage=(1.0 - (idle-prev_idle)*1.0/(total-prev_total))*100;
+		   	prev_idle=total_idle;
+		   	prev_total=total;
+	      	printk(KERN_INFO "In thread1 %ld %ld",idle,total);*/
+	      	continue;
+		}
 	}
-	kfree(buffer);
-	kfree(token);
-	sock_release(socket_ptr->sk_socket);
-	//printk(KERN_INFO "In thread1");
-	// struct file *f;
-
-	
 	return 0;
 }
 
@@ -238,6 +201,9 @@ static int simple_init (void) {
 /* exit function - logs that the module is being removed */
 static void simple_exit (void) {
 	int ret;
+	kfree(g.buffer);
+	kfree(g.token);
+	sock_release(socket_ptr->sk_socket);
 	hrtimer_cancel(&hr_timer);
  	ret = kthread_stop(thread1);
  	if(!ret)
