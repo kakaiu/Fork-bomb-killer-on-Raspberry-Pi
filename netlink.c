d582f91050809b139a261ba5a46ec667dd54943d
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-04-27 16:42:31 -0500

    lk

--- netlink.c
+++ netlink.c
@@ -151,12 +151,15 @@ static char * find_potential_fork_bomb(void) {
 	struct task_struct *task, *p;
 	struct list_head *pos;
 	int count=0;
+	int tmp;
 	printk("Hello,let begin\n");
 	task=&init_task;
 	list_for_each(pos,&task->tasks) {
 		p = list_entry(pos, struct task_struct, tasks);
 		count++;
-		while ((p=task_ppid_nr(p)) != 0) { //go through ancestors
+		printk("%d-->%d---->%s\n", task_ppid_nr(p), p->pid, p->comm);
+		for (tmp=task_ppid_nr(p); tmp!=0;) { //go through ancestors
+			p = get_pid_task(tmp, PIDTYPE_PID);
 			printk("%d-->%d---->%s\n", task_ppid_nr(p), p->pid, p->comm);
 		}
 	}
