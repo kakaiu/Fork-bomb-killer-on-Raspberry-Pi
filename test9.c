031f6aeba649b35ab84813ce1524438f64a5b666
Zhe Wang <zhewang.kakaiu@yahoo.com>
2019-04-28 01:30:37 -0500

    g

---
+++ test9.c
@@ -0,0 +1,28 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sched.h>
+#include <time.h>
+
+
+static int slowBranch(int cpuAffinity){
+	cpu_set_t set;
+	CPU_ZERO(&set);
+	CPU_SET(cpuAffinity, &set);
+	if (sched_setaffinity(getpid(), sizeof(set), &set) == -1){
+		exit(-1);
+	}
+	while(1) {
+		continue;
+	}
+
+	return 0;
+}
+
+int main( int argc, char* argv[] ){
+	slowBranch(1);
+	return 0;
+}
